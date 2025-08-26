385860af19c294f4ce65b535783fc31a88933837
jaenixm <jannik8501@gmail.com>
2025-08-26 11:50:01 +0200

    added yolo conda env

--- yolov12/run_yolo.py
+++ yolov12/run_yolo.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # 1) Load your trained weights
-model = YOLO("train_yolo_detect/weights/best.pt")  # or the path printed in your run
+model = YOLO("yolov12/pothole_yolov12_google_colab_training/weights/best.pt")  # or the path printed in your run
 
 # 2) Run on images, a folder, or a video
 results = model.predict(
-    source="/Users/jaenix/Documents/SAM2/pipeline/resources/sequence_to_test_1",
+    source="pipeline/resources/input_pictures",
     imgsz=1024,         
     conf=0.20,         
     iou=0.60,          
